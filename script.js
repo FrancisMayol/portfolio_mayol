01d28a1ea108d95639264bb48235010815d5bba4
Jeblanda <jblanda@mcm.edu.ph>
2026-02-03 22:16:21 +0800

    mayol

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Theme toggle
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 toggleButton.addEventListener('click', () => {
-   body.classList.toggle('dark');
-   body.classList.toggle('red');
+    body.classList.toggle('dark');
+    body.classList.toggle('red');
 });
 
 // Page flipping
@@ -14,19 +15,19 @@ const prevBtn = document.querySelector('.nav.prev');
 const nextBtn = document.querySelector('.nav.next');
 
 function updateBook() {
-   book.style.transform = `translateX(-${currentPage * 100}%)`;
+    book.style.transform = `translateX(-${currentPage * 100}%)`;
 }
 
 nextBtn.addEventListener('click', () => {
-   if (currentPage < pages.length - 1) {
-      currentPage++;
-      updateBook();
-   }
+    if (currentPage < pages.length - 1) {
+        currentPage++;
+        updateBook();
+    }
 });
 
 prevBtn.addEventListener('click', () => {
-   if (currentPage > 0) {
-      currentPage--;
-      updateBook();
-   }
+    if (currentPage > 0) {
+        currentPage--;
+        updateBook();
+    }
 });
